214f721150f30fa909b235e42344cb8b41fd6c2d
Samuel <samo.kobida@gmail.com>
2021-10-25 20:10:22 +0200

    autentification done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'is_admin'])->namespace('Admin')->prefix('admin')->group(function(){
+
+    Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class,'index'])->name('adminindex');
+});
+
+Auth::routes();
